df56f880f1b5bd6f502fdc88a8f9bfa2163872dc
Abrar-Ahmad036 <abrarahmad56261@gmail.com>
2026-06-29 17:19:50 +0500

    Add files via upload

---
+++ arguments.py
@@ -0,0 +1,11 @@
+def average(a,b):                         #required arguments
+     print("Average is : ",(a +b)/2)
+
+average(6,8)
+
+
+
+def average(a=5,b=7):                         #default arguments
+     print("Average is : ",(a +b)/2)
+
+average()
