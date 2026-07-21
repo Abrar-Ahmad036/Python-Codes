cf9bf85d3b3f02ddafb55e46653ff4e32c3e1cd5
Abrar-Ahmad036 <abrarahmad56261@gmail.com>
2026-07-21 15:05:28 +0500

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,33 @@
+# Question 1
+# numbers=(12,45,12,89,30)
+# print("12 present ",numbers.count(12)," times")
+# print("89 present on ",numbers.index(89)," index ")
+
+
+#Question 2
+# marks=(80,75,90)
+# new_marks=list(marks)
+# new_marks.append(85)
+# marks=tuple(new_marks)
+# print(marks)
+
+
+#Question 3
+laptop=("Dell","17","16GB")
+brand,processor,ram=laptop
+print("Brand :",brand)
+print("Processor : ",processor)
+print("Ram : ",ram)
+
+#Question 4
+# letters=("a","b","c","d","e","f","g")
+# print(letters[2:5:1])
+# print(letters[::-1])
+
+
+
+#Question 5
+# tuple1=(10,20,30)
+# tuple2=(40,50,60)
+# combines_tuple=tuple1+tuple2
+# print("total length is :",len(combines_tuple))
