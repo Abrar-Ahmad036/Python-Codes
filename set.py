60a0180fdfc51dd824021e0fa3f910acc8b5b804
Abrar-Ahmad036 <abrarahmad56261@gmail.com>
2026-08-02 14:57:12 +0500

    Add files via upload

---
+++ set.py
@@ -0,0 +1,19 @@
+#Question 1
+# [1,2,2,3,4,4,5]
+# number=set(numbers)
+# print("Unique items : ",number)
+
+
+#Question 2
+# fruits={"apple","banana","grapes"}
+# if "grapes" in fruits:
+#     print("Yes grapes is present ")
+# else:
+#     print("grapes not prsent ")
+
+
+#Question 3
+colors=set()
+colors.update("red","blue","green")
+colors.remove("blue")
+print(colors)
