c66d2ad38d46f8bebd63dbb4a5fa87c162f4b651
Abrar-Ahmad036 <abrarahmad56261@gmail.com>
2026-08-02 14:59:25 +0500

    Add files via upload

---
+++ import.py
@@ -0,0 +1,11 @@
+#Question 1 
+# import random
+# fruits=["Apple","Banana","mango","grapes","orange"]
+# print(random.choice(fruits))
+
+
+#Question 2
+from math import pow
+a=int(input("Enter 1st number : "))
+b=int(input("Enter 2nd number : "))
+print(pow(a,b))
